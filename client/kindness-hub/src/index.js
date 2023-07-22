2ae7775e1bd7980526df5d38a5ec0af32984f9f1
VardaQuraishi <vardaquraishi.vq@gmail.com>
2023-07-22 23:51:30 +0500

    font Playfair Display

--- client/kindness-hub/src/index.js
+++ client/kindness-hub/src/index.js
@@ -1,12 +1,12 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals'; // Import the reportWebVitals function
+import reportWebVitals from './reportWebVitals';
+import './global.css';
 
-import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
-
-const root = createRoot(document.getElementById('root')); // Use createRoot from react-dom/client
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +15,8 @@ root.render(
 
 // Call the reportWebVitals function to measure performance metrics
 reportWebVitals(console.log); // You can pass a function to log the results or use your preferred analytics endpoint
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
