456aaede2cedee2753061650eb89e7bee84f1701
VardaQuraishi <vardaquraishi.vq@gmail.com>
2023-07-24 23:47:32 +0500

    fetching acts

--- client/kindness-hub/src/components/ListOfRandomActs.js
+++ client/kindness-hub/src/components/ListOfRandomActs.js
@@ -1,13 +1,47 @@
 // ListOfRandomActs.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ListOfRandomActs = () => {
+  const [actsOfKindness, setActsOfKindness] = useState([]);
+
+  useEffect(() => {
+    // Fetch all acts of kindness from the server when the component mounts
+    fetch('/api/acts-of-kindness', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setActsOfKindness(data);
+      })
+      .catch((error) => {
+        console.log('Error fetching acts of kindness:', error);
+      });
+  }, []);
+  
+
   return (
     <div>
       <h1>List of Random Acts</h1>
-      {/* Your content for the ListOfRandomActs page goes here */}
+      <ul>
+        {actsOfKindness.map((act) => (
+          <li key={act._id}>
+            <h2>{act.title}</h2>
+            <p>Category: {act.category}</p>
+            <p>{act.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+  
 };
 
 export default ListOfRandomActs;
