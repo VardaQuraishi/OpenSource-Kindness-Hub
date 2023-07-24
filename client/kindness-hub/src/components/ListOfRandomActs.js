54b4d206447c83cce4efad7de9c2797f24e63e0e
VardaQuraishi <vardaquraishi.vq@gmail.com>
2023-07-24 23:57:32 +0500

    Added Filters and Search Bar

--- client/kindness-hub/src/components/ListOfRandomActs.js
+++ client/kindness-hub/src/components/ListOfRandomActs.js
@@ -1,19 +1,10 @@
-// ListOfRandomActs.js
 import React, { useState, useEffect } from 'react';
 
 const ListOfRandomActs = () => {
   const [actsOfKindness, setActsOfKindness] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  // Define the number of acts to show per page
-  const actsPerPage = 2;
-
-  // Calculate the starting and ending indices for the current page
-  const startIndex = (currentPage - 1) * actsPerPage;
-  const endIndex = startIndex + actsPerPage;
-
-
-
-
+  const [selectedFilter, setSelectedFilter] = useState('all');
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     // Fetch all acts of kindness from the server when the component mounts
@@ -37,23 +28,71 @@ const ListOfRandomActs = () => {
       });
   }, []);
 
+  // Define the number of acts to show per page
+  const actsPerPage = 2;
+
+  // Calculate the starting and ending indices for the current page
+  const startIndex = (currentPage - 1) * actsPerPage;
+  const endIndex = startIndex + actsPerPage;
+
+  // Use the filter method to show acts that match the selected filter
+  const filteredActs = actsOfKindness.filter((act) => {
+    if (selectedFilter === 'all') {
+      return true;
+    }
+    return act.category === selectedFilter;
+  });
+
+  // Use the filter method to show acts that match the search query
+  const searchedActs = filteredActs.filter((act) => {
+    if (searchQuery === '') {
+      return true;
+    }
+    // Perform search based on act title or description, for example
+    return act.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+           act.description.toLowerCase().includes(searchQuery.toLowerCase());
+  });
 
   return (
     <div>
       <h1>List of Random Acts</h1>
+      <div>
+        {/* Search Bar */}
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search acts by title or description"
+        />
+        {/* Filter Dropdown */}
+        <select value={selectedFilter} onChange={(e) => setSelectedFilter(e.target.value)}>
+          <option value="all">All</option>
+          {/* Add other filter options here */}
+        </select>
+      </div>
       <ul>
-        {actsOfKindness.slice(startIndex, endIndex).map((act) => (
+        {searchedActs.slice(startIndex, endIndex).map((act) => (
           <li key={act._id}>
             <h2>{act.title}</h2>
             <p>Category: {act.category}</p>
             <p>{act.description}</p>
-          </li>))}
+          </li>
+        ))}
       </ul>
-      <button onClick={() => setCurrentPage(currentPage - 1)}>Previous Page</button>
-      <button onClick={() => setCurrentPage(currentPage + 1)}>Next Page</button>
+      {/* Pagination */}
+      <div>
+        <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+          Previous Page
+        </button>
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={endIndex >= searchedActs.length}
+        >
+          Next Page
+        </button>
+      </div>
     </div>
   );
-
 };
 
 export default ListOfRandomActs;
