bca6519ae4d8b38ff2f2dcd14b6ef8256b0e9668
VardaQuraishi <vardaquraishi.vq@gmail.com>
2023-07-24 23:51:01 +0500

    pagination

--- client/kindness-hub/src/components/ListOfRandomActs.js
+++ client/kindness-hub/src/components/ListOfRandomActs.js
@@ -3,6 +3,17 @@ import React, { useState, useEffect } from 'react';
 
 const ListOfRandomActs = () => {
   const [actsOfKindness, setActsOfKindness] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  // Define the number of acts to show per page
+  const actsPerPage = 2;
+
+  // Calculate the starting and ending indices for the current page
+  const startIndex = (currentPage - 1) * actsPerPage;
+  const endIndex = startIndex + actsPerPage;
+
+
+
+
 
   useEffect(() => {
     // Fetch all acts of kindness from the server when the component mounts
@@ -25,23 +36,24 @@ const ListOfRandomActs = () => {
         console.log('Error fetching acts of kindness:', error);
       });
   }, []);
-  
+
 
   return (
     <div>
       <h1>List of Random Acts</h1>
       <ul>
-        {actsOfKindness.map((act) => (
+        {actsOfKindness.slice(startIndex, endIndex).map((act) => (
           <li key={act._id}>
             <h2>{act.title}</h2>
             <p>Category: {act.category}</p>
             <p>{act.description}</p>
-          </li>
-        ))}
+          </li>))}
       </ul>
+      <button onClick={() => setCurrentPage(currentPage - 1)}>Previous Page</button>
+      <button onClick={() => setCurrentPage(currentPage + 1)}>Next Page</button>
     </div>
   );
-  
+
 };
 
 export default ListOfRandomActs;
