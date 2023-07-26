abbd3689dd3ce3c801a6a68481ba4b66703c7fe2
VardaQuraishi <vardaquraishi.vq@gmail.com>
2023-07-27 00:09:15 +0500

    Model style1

--- client/kindness-hub/src/components/Modal.js
+++ client/kindness-hub/src/components/Modal.js
@@ -1,21 +1,43 @@
 // Modal.js
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FaTimes } from 'react-icons/fa';
-import '../styles/Modal.css'; // Create a new CSS file for styling the modal
+import '../styles/Modal.css';
+
+const Tag = ({ text }) => {
+  return <div className="tag">{text}</div>;
+};
 
 const Modal = ({ act, onClose }) => {
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (event.target.classList.contains('modal-container')) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [onClose]);
+
   return (
     <div className="modal-container">
       <div className="modal-content">
         <button className="modal-close-button" onClick={onClose}>
           <FaTimes />
         </button>
+        <img className="modal-image" src={act.image} alt={act.title} />
         <h2>{act.title}</h2>
-        <p>Category: {act.category}</p>
-        <p>Description: {act.description}</p>
-        <img src={act.image} alt={act.title} />
-        <p>Tags: {act.tags.join(', ')}</p>
+        <p>Category | {act.category}</p>
+        <p>{act.description}</p>
+        <div className="tags-container">
+          {act.tags.map((tag, index) => (
+            <Tag key={index} text={tag} />
+          ))}
+        </div>
 
         {/* Add attractive styling for the "Let's Do It" button */}
         <button className="lets-do-it-button">Let's Do It</button>
